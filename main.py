b10b05d9d143b93a1b133d61abf0d896e9015608
DSSanjaya5 <dssanjay55555@gmail.com>
2024-10-06 22:59:45 +0530

    FAD: Added rotate feature and updated other py filenames

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from antigem.split_pdf import SplitPDF
-from antigem.merge_pdf import MergePDF
+from antigem.splitter import SplitPDF
+from antigem.merger import MergePDF
+from antigem.rotater import RotatePDF
 
 if __name__ == '__main__':
-    mp = MergePDF()
+    mp = RotatePDF(path='../test_pdf.pdf')
     breakpoint()
